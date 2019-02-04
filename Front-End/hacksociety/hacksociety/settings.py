ae2ba6f746d7df1472b8d48ba89735727f736452
kpark2 <kpark2@caltech.edu>
2019-02-03 23:11:53 -0800

    base template and nav bar

--- Front-End/hacksociety/hacksociety/settings.py
+++ Front-End/hacksociety/hacksociety/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'hacksociety.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'hacksociety')],
+        'DIRS': [os.path.join(BASE_DIR, 'hacksociety/templates/hacksociety')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
