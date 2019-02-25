dc4573683f618991c5d7ca7b594a6ecf4587cf20
Zafir Abou-Zamzam <z.abouzamzam@gmail.com>
2019-02-24 21:33:39 -0800

    Add files for map url

--- Front-End/hacksociety/home/views.py
+++ Front-End/hacksociety/home/views.py
@@ -5,3 +5,6 @@ def index(request):
     #template = loader.get_template('home/index.html')
     context = {}
     return render(request, 'home/index.html', context)
+
+def map(request):
+    return render(request, 'home/map.html', {})
